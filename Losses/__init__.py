c090070884f5898ff8d6998afc575593ca320a5c
chirag gupta <103719146+chiruu12@users.noreply.github.com>
2024-09-09 19:23:01 +0530

    updating files

--- Losses/__init__.py
+++ Losses/__init__.py
@@ -4,4 +4,4 @@ from .cosine_similarity_loss import CosineSimilarityLoss
 from .poisson_loss import PoissonLoss
 from .huber_loss import HuberLoss
 from .mean_squared_error_loss import MSE
-from .mean_absolute_error_loss import MSE
+from .mean_absolute_error_loss import MAE
